15c4a77674ae3851419a42121469591200f6e8f4
Ivan Shakhorski <ivanwanwas@gmail.com>
2021-01-15 14:51:55 +0300

    make user store

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,100 @@
+import axios from "axios";
 export default {
   state: {
     user: {},
   },
   actions: {
-    login() {},
-    register() {},
-    forgot() {},
-    reset() {},
-    logout() {},
-    updateProfile() {},
+    login(ctx, user) {
+      return new Promise((resolve, reject) => {
+        const body = { ...user };
+        const headers = {
+          "Content-Type": "application/json"
+        };
+        axios
+          .post("http://localhost:5000/auth/login", body, {headers})
+          .then(async (response) => {
+            const data = await response.data();
+            ctx.commit("setUser", data);
+            localStorage.setItem("token", data.token);
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    register(ctx, user) {
+      return new Promise((resolve, reject) => {
+        const body = { ...user };
+        const headers = {
+          "Content-Type": "application/json"
+        };
+        axios
+          .post("http://localhost:5000/auth/register", body, { headers })
+          .then(async () => {
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    forgot(ctx, email) {
+      return new Promise((resolve, reject) => {
+        const body = { email };
+        const headers = {
+          "Content-Type": "application/json"
+        };
+        axios
+          .post("http://localhost:5000/auth/forgot", body, { headers })
+          .then(async () => {
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    reset(ctx, password) {
+      return new Promise((resolve, reject) => {
+        const body = { password };
+        const headers = {
+          "Content-Type": "application/json"
+        };
+        axios
+          .post("http://localhost:5000/auth/reset", body, { headers })
+          .then(async () => {
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    logout(ctx) {
+      ctx.commit("setUser", {});
+      localStorage.removeItem("token");
+    },
+    updateProfile(ctx, user) {
+      return new Promise((resolve, reject) => {
+        const body = { ...user };
+        const headers = {
+          "Authorization": ctx.getters.getUser.token,
+          "Content-Type": "application/json"
+        };
+        axios
+          .patch(`http://localhost:5000/users/${user._id}`, body, { headers })
+          .then(async (response) => {
+            const data = await response.data();
+            ctx.commit("setUser", data);
+            localStorage.setItem("token", data.token);
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
   },
   mutations: {
     setUser(state, newUser) {
